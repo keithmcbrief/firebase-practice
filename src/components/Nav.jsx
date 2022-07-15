74a4f6d038343de7a847b288cc5fbf29f6c7db8b
keithmcbrief <yurij97@gmail.com>
2022-07-15 00:06:46 -0500

    skeleton loading

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,15 +13,6 @@ const Nav = () => {
   const [user, setUser] = React.useState(null);
   const [loading, setLoading] = React.useState(true);
 
-  React.useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setLoading(false);
-      if (user) {
-        setUser(user);
-      }
-    });
-  }, []);
-
   function register() {
     console.log("register");
     createUserWithEmailAndPassword(auth, "email@email.com", "test123")
@@ -46,6 +37,15 @@ const Nav = () => {
     setUser(null);
   }
 
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, []);
+
   return (
     <div className="nav">
       <div className="nav__container">
@@ -55,17 +55,18 @@ const Nav = () => {
             <img className="logo__img" src={Logo} alt=""></img>
           </figure>
         </div>
-        <div className="nav__skeletons">
+        {/* <div className="nav__skeletons">
           <div className="nav__skeleton--button"></div>
           <div className="nav__skeleton--circle"></div>
-        </div>
+        </div> */}
         <div className="nav__links">
           {user && (
             <button className="btn btn__logout" onClick={logout}>
-              E
+              {user.email[0]}
             </button>
           )}
-          {!user && (
+
+          {!user && !loading && (
             <>
               <button className="btn btn__register" onClick={register}>
                 Register
@@ -76,8 +77,12 @@ const Nav = () => {
             </>
           )}
 
-          {loading ? "loading..." : user.email}
-          {/* <button className="btn user__letter">Y</button> */}
+          {loading && (
+            <div className="nav__skeletons">
+              <div className="nav__skeleton--button"></div>
+              <div className="nav__skeleton--circle"></div>
+            </div>
+          )}
         </div>
       </div>
     </div>
