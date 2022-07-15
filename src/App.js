ebaf67194ca7541a5959cc558ccc5792742d90e7
keithmcbrief <yurij97@gmail.com>
2022-07-14 23:07:04 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
 import React from 'react'
 import "./App.css";
-import { auth } from "./firebase/init";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-} from "firebase/auth";
 import Nav from './components/Nav'
+// import { auth } from "./firebase/init";
+// import {
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+//   signOut,
+//   onAuthStateChanged,
+// } from "firebase/auth";
 
 function App() {
-  const [user, setUser] = React.useState({})
-  const [loading, setLoading] = React.useState(true)
+  // const [user, setUser] = React.useState({})
+  // const [loading, setLoading] = React.useState(true)
 
-  React.useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setLoading(false)
-      if (user) {
-        setUser(user)
-      }
-    })
-  },[])
+  // React.useEffect(() => {
+  //   onAuthStateChanged(auth, (user) => {
+  //     setLoading(false)
+  //     if (user) {
+  //       setUser(user)
+  //     }
+  //   })
+  // },[])
 
-  function register() {
-    console.log("register");
-    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
-      .then((user) => {
-        console.log(user)
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+  // function register() {
+  //   console.log("register");
+  //   createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+  //     .then((user) => {
+  //       console.log(user)
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
 
-  function login() {
-    signInWithEmailAndPassword(auth, "email@email.com", "test123")
-    .then(({ user }) => {
-      setUser(user)
-    })
-    .catch((error) => {
-    });
-  }
+  // function login() {
+  //   signInWithEmailAndPassword(auth, "email@email.com", "test123")
+  //   .then(({ user }) => {
+  //     setUser(user)
+  //   })
+  //   .catch((error) => {
+  //   });
+  // }
 
-  function logout() {
-    signOut(auth)
-    setUser({})
-  }
+  // function logout() {
+  //   signOut(auth)
+  //   setUser({})
+  // }
 
   return (
     <div className="App">
       <Nav />
-      <button onClick={register}>Register</button>
+      {/* <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
       <button onClick={logout}>Logout</button>
-      {loading ? 'loading...' : user.email}
+      {loading ? 'loading...' : user.email} */}
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,75 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Logo from "../assets/logo.png"
+import Logo from "../assets/logo.png";
+import { auth } from "../firebase/init";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 const Nav = () => {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, [user]);
+
+  function register() {
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function login() {
+    signInWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then(({ user }) => {
+        setUser(user);
+      })
+      .catch((error) => {});
+  }
+
+  function logout() {
+    signOut(auth);
+    setUser({});
+  }
+
   return (
     <div className="nav">
-      <FontAwesomeIcon icon="bars" />
-      <img className="logo" src={Logo}></img>
+      <div className="nav__container">
+        <div className="logo__wrapper">
+          <FontAwesomeIcon icon="bars" className="btn btn__bars" />
+          <figure className="logo">
+            <img className="logo__img" src={Logo} alt=""></img>
+          </figure>
+        </div>
+        <div className="nav__links">
+          {
+            user.length > 0 ?
+            'logged in' :
+            <>
+            <button className="btn btn__register" onClick={register}>Register</button>
+            <button className="btn btn__login" onClick={login}>Login</button>
+            logged out
+            </>
+          }
+          
+          
+          {/* {loading ? "loading..." : user.email} */}
+        </div>
+      </div>
     </div>
   );
 };
